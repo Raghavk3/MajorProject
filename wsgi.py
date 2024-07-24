c66a3ec36a608fbb35941fb41b57fe73985113fc
Raghava Kesanapalli <92518774+Raghavk3@users.noreply.github.com>
2024-07-24 22:11:33 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for detecting_mental_disorders
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'detecting_mental_disorders.settings')
+application = get_wsgi_application()
